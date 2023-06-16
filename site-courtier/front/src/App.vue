a43461e6e00563a48209e013989ad1ba2c5cc705
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2023-06-16 16:07:48 +0200

    front register ongoing

--- site-courtier/front/src/App.vue
+++ site-courtier/front/src/App.vue
@@ -1,7 +1,10 @@
 <template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/register">Register</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ site-courtier/front/src/views/RegisterView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container card" style="padding: 2%;width: 40%;background-color: #588B8B;">
+        <form class="row g-5" action="/login">
+            <div class="col-12">
+                <p class="h3">Register</p>
+            </div>
+            <div class="col-md-6">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="inputFirstNameLG" placeholder="First name">
+                    <label for="inputFirstNameLG">First name</label>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="inputLastNameLG" placeholder="Last name">
+                    <label for="inputLastNameLG">Last name</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="inputEmailLG" placeholder="Email">
+                    <label for="inputEmailLG">Email</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="inputPasswordLG" placeholder="Password">
+                    <label for="inputPassword">Password</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary fs-4" style="background-color: #E9C46A;border-color: #588B8B;color: black;">Sign in</button>
+            </div>
+        </form>
+    </div>
+</template>
