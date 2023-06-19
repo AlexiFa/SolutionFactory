eccc77c242421d7131aed052c8942cdb7b7038dd
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2023-06-19 11:36:32 +0200

    login front commencé

--- site-courtier/front/src/views/LoginView.vue
+++ site-courtier/front/src/views/LoginView.vue
@@ -1,3 +1,24 @@
 <template>
-    <div class="btn btn-success">log in</div>
+    <div class="container card" style="padding: 2%;width: 40%;background-color: #588B8B;">
+        <form class="row g-5" action="/">
+            <div class="col-12">
+                <p class="h3">Login</p>
+            </div>
+            <div class="col-12">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="inputEmailLG" placeholder="Email">
+                    <label for="inputEmailLG">Email</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="inputPasswordLG" placeholder="Password">
+                    <label for="inputPassword">Password</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary fs-4" style="background-color: #E9C46A;border-color: #588B8B;color: black;">Sign in</button>
+            </div>
+        </form>
+    </div>
 </template>
