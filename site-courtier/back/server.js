62a49c8061262e4980e4b1432305199c1befca71
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-06-27 12:09:28 +0200

    back end register and login

--- site-courtier/back/server.js
+++ site-courtier/back/server.js
@@ -1,59 +1,156 @@
 import express from 'express';
-//import stythc
-// import {envs, Client} from 'stytch';
+// import stythc;
+import {envs, Client} from 'stytch';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { json } from 'sequelize';
-import bodyParser from "body-parser";
+// import { json } from 'sequelize';
+// import bodyParser from "body-parser";
 
 dotenv.config()
 
 const app = express()
+
+
+// app.use(bodyParser.json())
+
+
+
+
+
+
+// app.post('/register', (req,res) => {
+//     res.send({
+//         message: `Hello ${req.body.email}`
+        
+//     })
+// })
+
+// app.get('/status', (req, res) => {
+//     res.send({
+//         message: `Hello`
+//     })
+// })
+
+
+//stych
+
+const client = new Client({
+    project_id: process.env.PROJECT_ID,
+    secret: process.env.SECRET,
+    env: envs.test
+})
+
+const port = process.env.PORT || 3000;
+
 app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json())
+app.use(express.json());
 
+app.post('/register', async (req, res) => {
+    const{ email, password} = req.body;
 
+    try{
+        const resp = await client.passwords.create({
+            email,
+            password,
+            session_duration_minutes: 60
+        })
+
+        res.json({
+            success: true,
+            message: 'User created successfuly ',
+            token: resp.session_token
+        })
+    }catch (err) {
+        console.log(err);
+
+        res.json({
+            success: false,
+            messgae: err.error_message,
+            err: err
+        })
+    }
+
+})
+
+app.post('/login', async (req, res) => {
+    const{ email, password} = req.body;
+
+    try{
+        const resp = await client.passwords.authenticate({
+            email,
+            password,
+            session_duration_minutes: 60
+        })
+
+        res.json({
+            success: true,
+            message: 'User loged in successfuly ',
+            token: resp.session_token
+        })
+    }catch (err) {
+        console.log(err);
+
+        res.json({
+            success: false,
+            messgae: err.error_message,
+            err: err
+        })
+    }
+
+})
+
+app.post('/authenticate', async (req, res) => {
+    const{ session_token} = req.body;
+
+    try{
+        await client.sessions.authenticate({
+            session_token
+        })
+
+        res.json({
+            success: true,
+            message: 'Token is valid ',
+        })
+    }catch (err) {
+        console.log(err);
+
+        res.json({
+            success: false,
+            messgae: err.error_message,
+            err: err
+        })
+    }
+
+})
+
+app.post('/logout', async (req, res) => {
+    const{ session_token} = req.body;
+
+    try{
+        await client.sessions.revoke({
+            session_token
+        })
+
+        res.json({
+            success: true,
+            message: 'Succesfuly log out ',
+        })
+    }catch (err) {
+        console.log(err);
+
+        res.json({
+            success: false,
+            messgae: err.error_message,
+            err: err
+        })
+    }
+
+})
 
 app.listen(3000, () =>{
     console.log("server has start")
 })
 
 
-app.post('/register', (req,res) => {
-    res.send({
-        message: `Hello ${req.body.email}`
-        
-    })
-})
-
-app.get('/status', (req, res) => {
-    res.send({
-        message: `Hello`
-    })
-})
 
 
-//stych
-// app.post('/register', async (req, res) => {
-//     const{ email, password} = req.body;
-
-//     try{
-//         const resp = await client.passwords.create({
-//             email,
-//             password,
-//             session_duration_minutes: 60
-//         })
-
-//         res.jason({
-//             success: 
-//         })
-//     }
-
-// })
-
-// const client = new Client({
-//     project_id: process.env.PROJECT_ID,
-//     secret: process.env.SECRET,
-//     env: envs.test
-// })
--- site-courtier/front/src/views/RegisterView.vue
+++ site-courtier/front/src/views/RegisterView.vue
@@ -1,14 +1,54 @@
+<script setup>
+
+import {ref} from 'vue' 
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const email = ref('')
+const password = ref('')
+const conf_password = ref('')
+
+const Register = async () => {
+    if (!email.value || !password.value || !conf_password.value){
+        return alert('Please fill in all fields')
+    }
+
+    if (password.value != conf_password.value){
+        return alert('Password do not match')
+    }
+
+    const res  = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    }).then(res => res.json())
+    if (res.success) {
+        localStorage.setItem('token', res.token)
+        router.push('/')
+    } else{
+        alert(res.message)
+    }
+}
+
+</script>
+
 <template>
     <!--<div class="ellipse1"></div>
     <div class="ellipse2"></div>-->
     <div class="container card" style="padding: 1%;background-color: #2A2D34;">
         <div class="row">
         <div class="container card col" style="padding: 2%;width: 40%;background-color: #588B8B;margin: 1%;">
-            <form class="row g-5" action="/login">
+            <form class="row g-5" @submit.prevent="Register">
             <div class="col-12 mar2">
                 <p class="h1">Register</p>
             </div>
-            <div class="col-md-6 mar2">
+            <!-- <div class="col-md-6 mar2">
                 <div class="form-floating">
                     <input type="text" class="form-control" id="inputFirstNameLG" placeholder="First name">
                     <label for="inputFirstNameLG">First name</label>
@@ -19,21 +59,27 @@
                     <input type="text" class="form-control" id="inputLastNameLG" placeholder="Last name">
                     <label for="inputLastNameLG">Last name</label>
                 </div>
-            </div>
+            </div> -->
             <div class="col-12 mar2">
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="inputEmailLG" placeholder="Email">
+                    <input type="email" v-model="email" class="form-control" id="inputEmailLG" placeholder="Email">
                     <label for="inputEmailLG">Email</label>
                 </div>
             </div>
             <div class="col-12 mar2">
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="inputPasswordLG" placeholder="Password">
+                    <input type="password" v-model="password" class="form-control" id="inputPasswordLG" placeholder="*************">
                     <label for="inputPassword">Password</label>
                 </div>
             </div>
+            <div class="col-12 mar2">
+                <div class="form-floating">
+                    <input type="password" v-model="conf_password" class="form-control" id="inputPasswordLG" placeholder="*************">
+                    <label for="inputPassword">Confirm Password</label>
+                </div>
+            </div>
             <div class="col-12">
-                <button type="submit" class="btn btn-primary fs-2" style="background-color: #E9C46A;border-color: #588B8B;color: black;">Sign up</button>
+                <button type="submit" value="Register" class="btn btn-primary fs-2" style="background-color: #E9C46A;border-color: #588B8B;color: black;">Sign up</button>
             </div>
         </form>
         <router-link to="/login" style="margin-top: 3%;color: black;">already have an account ?</router-link>
