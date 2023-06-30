c6d78fba087d726fa55a947ec1fda0d5340b0ba9
Becca Morel <rebk@MacBook-Pro-de-Becca.local>
2023-06-30 10:06:19 +0200

    ajout du front menu courtier

--- site-courtier/front/src/components/Dashboard/DCardDossier.vue
+++ site-courtier/front/src/components/Dashboard/DCardDossier.vue
@@ -1,12 +1,16 @@
 <template>
-    <div style="border:2px solid black; display: flex; padding:5%;width:100%;background-color: white;">
-        <div style="display: flex; flex-direction: column;">
-            <h3>Client: </h3>
-            <h4>Prêt demandé: </h4>
+    <div style="display: flex; justify-content: space-between; width:100%; height:100%; padding:0%5%; border:2px solid black; background-color: white; border-radius: 20px;">
+        <div style="display: flex; flex-direction: column;align-items: flex-start; padding:4%;">
+            <h3 style="padding-bottom: 2%;">Client: </h3>
+            <h3 style="padding-top: 2%;">Prêt demandé:</h3>
         </div>
-        <div style="display: flex; flex-direction: column;">
-            <div style="height: 100%; background-color:black ;"><img src="" alt=""></div>
-            <h5>Afficher les documents</h5>
+        <div style="display: flex; flex-direction: column; justify-content: space-around; align-items:center ;padding:1%;">
+            <div style="background-color:black; height:70%; width: 55%;">
+                <img src="" alt="">
+            </div>
+            <div style="text-decoration: underline;">
+                <h5>Afficher les documents</h5>
+            </div>
         </div>
     </div>
 </template>
--- site-courtier/front/src/views/courtier/CDashboard.vue
+++ site-courtier/front/src/views/courtier/CDashboard.vue
@@ -1,10 +1,10 @@
 <template>
     <div style="background-color: #588B8B;padding: 5%;" >
-      <div name="Dossier" style="display: flex; border : 1px solid black;">
-          <div style="width:20%;">
+      <div name="Dossier-Client" style="display: flex; align-items: center;justify-content: space-between;">
+          <div style="width:25%;">
               <DCardSimple></DCardSimple>
           </div>
-          <div style="width:60%;">
+          <div style="width:70%;">
               <DCardDossier></DCardDossier>
           </div>   
       </div>
