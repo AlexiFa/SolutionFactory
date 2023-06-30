595004af99e4dc1a2ac779238cc4b68e2310848a
Becca Morel <rebk@MacBook-Pro-de-Becca.local>
2023-06-30 12:32:46 +0200

    commit b4 coming home

--- site-courtier/front/src/components/Dashboard/DCardSuivi.vue
+++ site-courtier/front/src/components/Dashboard/DCardSuivi.vue
@@ -1,37 +1,15 @@
 <template>
-    <div style="display: flex;flex-direction:column; justify-content:space-evenly; border:2px solid black;border-radius: 20px;">
+    <div style="display: flex;flex-direction:column; align-items: center; justify-content:space-evenly; border:2px solid black;border-radius: 20px;">
       <h4 style="">Stade d'avancement:</h4>
-      <div class="progress" style="">
-        <div class="progress-bar progress-bar-consulted" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75" :style="{ width: progress1 + '%' }">Consulté</div>
-        <div class="progress-bar progress-bar-send" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75" :style="{ width: progress2 + '%' }">Envoyé</div>
-        <div class="progress-bar progress-bar-done" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75" :style="{ width: progress3 + '%' }">Terminé</div>
+      <div class="progress" style="width: 90%;">
+        <div class="progress-bar progress-bar-consulted" role="progressbar" style="width: 33%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75">Consulté</div>
+        <div class="progress-bar progress-bar-send" role="progressbar" style="width: 34%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75">Envoyé</div>
+        <div class="progress-bar progress-bar-done" role="progressbar" style="width: 33s%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="75">Terminé</div>
       </div>
     </div>
   
   </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        progress1: 0,
-        progress2: 0,
-        progress3: 0,
-      };
-    },
-    methods: {
-      Consulted() {
-        this.progress1 = 15;
-      },
-      Send() {
-        this.progress2 = 30;
-      },
-      Done() {
-        this.progress3 = 60;
-      },
-    },
-  };
-  </script>
+
   
   <style scoped>
   
