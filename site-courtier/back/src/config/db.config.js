edc81418f8a2492aaafad8c8dfe7904fc11f3210
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-07-03 12:01:43 +0200

    Add seed to the database

--- site-courtier/back/src/config/db.config.js
+++ site-courtier/back/src/config/db.config.js
@@ -10,8 +10,9 @@
 
 
 import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('solution_factory', 'root', 'root', {
+//Il faut modifier le mot de passe selon le mot de passe de MySqlWorkBench sur vos machine.
+//Remplacez ou y a le texte: iTunes101020
+const sequelize = new Sequelize('solution_factory', 'root', 'iTunes101020', {
   host: 'localhost',
   dialect: 'mysql',
 });
